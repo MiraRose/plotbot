abee6c8a6d2d951e19989ef2fd7d984bf8917d0c
MiraRose <miraastar@gmail.com>
2021-07-11 19:12:33 -0400

    Added html page and started working on putting in random character button and response

--- charactergen/views.py
+++ charactergen/views.py
@@ -1,9 +1,16 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
 
 from charactergen.models import Character
 
 
 def index(request):
+    template = loader.get_template('charactergen/index.html')
+    context = {'random_character': 'blah', }
+    return HttpResponse(template.render(context, request))
 
+
+def get_random_character(request):
     character = Character.objects.random()
-    return HttpResponse(character.type)
+    return HttpResponseRedirect(reverse('index'), {'random_character': character})
