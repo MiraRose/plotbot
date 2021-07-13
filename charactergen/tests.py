ee401f5fa6ca5f3f40b6be8f272858f41bd7d3cf
MiraRose <miraastar@gmail.com>
2021-07-12 20:13:34 -0400

    Some really basic styling, can show character images based on character type, test for get random character, actually shows a random character on the page now

--- charactergen/tests.py
+++ charactergen/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from charactergen.models import Character, Genre
+
+description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget " \
+              "nulla ipsum. Cras odio tortor, commodo imperdiet facilisis eu, vulputate sed " \
+              "elit. Curabitur pulvinar congue lectus vitae facilisis. In et quam tempor, imperdiet " \
+              "risus ac, finibus ipsum. Phasellus sodales maximus ante, eu bibendum ipsum tincidunt et. " \
+              "Ut id mauris malesuada, rutrum odio nec, varius nisi. Nunc ut tincidunt lorem. Integer " \
+              "quis bibendum lorem. Nam et consectetur felis, vel eleifend velit."
+
+
+def create_test_character(character_type):
+    genre = Genre.objects.create(name="Fantasy", description=description)
+    return Character.objects.create(type=character_type, description=description, genre=genre)
+
+
+class CharactergenTests(TestCase):
+
+    def test_random_character_shows_character(self):
+        conversation = create_test_character("Meep")
+        url = reverse('get_random_character')
+        response = self.client.post(url, follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Meep")
--- charactergen/views.py
+++ charactergen/views.py
@@ -7,10 +7,11 @@ from charactergen.models import Character
 
 def index(request):
     template = loader.get_template('charactergen/index.html')
-    context = {'random_character': 'blah', }
+    character = Character.objects.random()
+    context = {'random_character': character}
     return HttpResponse(template.render(context, request))
 
 
 def get_random_character(request):
     character = Character.objects.random()
-    return HttpResponseRedirect(reverse('index'), {'random_character': character})
+    return HttpResponseRedirect(reverse('index', args=()), {'random_character': character})
